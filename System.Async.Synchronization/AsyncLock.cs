d60aa893e2bf67efd40daccfcebe16276e9af31a
bazmandude <garyrday@hotmail.com>
2016-12-12 15:12:29 +0000

    Added async friendly lock

---
+++ System.Async.Synchronization/AsyncLock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Async.Synchronization
+{
+    /// Task lock.
+    /// Won't cause a thread pool thread to be hogged while wait to the synchronization object to be signaled
+    /// based on Stephen Toub's article below
+    /// </summary>
+    /// <see cref="https://blogs.msdn.microsoft.com/pfxteam/2012/02/12/building-async-coordination-primitives-part-6-asynclock/"/>
+    public class AsyncLock
+    {
+        private readonly AsyncSemaphore _semaphore;
+        private readonly Task<Releaser> _releaser;
+
+        /// <summary>
+        /// Lock releaser. Disposable object that releases the lock when disposed.
+        /// </summary>
+        /// <example>
+        /// private readonly AsyncLock _lock = new AsyncLock();  
+        /// using(var releaser = await _lock.LockAsync()) 
+        /// { 
+        ///     ...protected code here 
+        /// }
+        /// </example>
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock _toRelease;
+
+            internal Releaser(AsyncLock toRelease) { _toRelease = toRelease; }
+
+            public void Dispose()
+            {
+                if (_toRelease != null)
+                {
+                    _toRelease._semaphore.Release();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Waits for the lock to be released
+        /// </summary>
+        public Task<Releaser> LockAsync()
+        {
+            var wait = _semaphore.WaitAsync();
+            return wait.IsCompleted 
+                ? _releaser 
+                : wait.ContinueWith((_, state) => new Releaser((AsyncLock)state), this, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+    }
+}
