fbc804b76d7e303c6c9a3c68a63907a2cac30326
Gary <garyrday@outlook.com>
2016-12-18 18:05:26 +0000

    Added test project and started adding MSpec tests

---
+++ Susyem.Async.Synchronization.Tests/AsyncLockTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Machine.Specifications;
+using System.Async.Synchronization;
+
+namespace Susyem.Async.Synchronization.Tests
+{
+    [Subject(typeof(AsyncLock))]
+    public class When_acquiring_a_free_lock
+    {
+        protected static AsyncLock Subject;
+        protected static AsyncLock.Releaser Result;
+
+        Establish context = () =>
+        {
+            Subject = new AsyncLock();
+        };
+
+        Cleanup after = () =>
+        {
+            Result.Dispose();
+        };
+
+        Because of = async () =>
+        {
+            Result = await Subject.LockAsync();
+        };
+
+        It should_acquire_lock_immediately = () => Result.ShouldNotBeNull();
+    }
+}
--- Susyem.Async.Synchronization.Tests/Properties/AssemblyInfo.cs
+++ Susyem.Async.Synchronization.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("System.Async.Synchronization")]
-[assembly: AssemblyDescription(".NET async/await friendly task synchronization primitives")]
+[assembly: AssemblyTitle("Susyem.Async.Synchronization.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("System.Async.Synchronization")]
+[assembly: AssemblyProduct("Susyem.Async.Synchronization.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4cdc69d-dca3-41dc-9de3-22c6b77fced7")]
 
 // Version information for an assembly consists of the following four values:
 //
