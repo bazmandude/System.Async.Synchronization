57f39f4ff12609ba1942936d2217a2162bdbedd1
Gary <garyrday@outlook.com>
2016-12-19 12:53:14 +0000

    Added async collection project to solution

---
+++ System.Async.Collections/AsyncQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Async.Collections.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Async.Collections
+{
+    public class AsyncQueue<T> : IAsyncQueue<T>
+    {
+        private Queue<T> _queue;
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public AsyncQueue()
+        {
+            _queue = new Queue<T>();
+        }
+
+        public Task<T> Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ System.Async.Collections/Interfaces/IAsyncQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Async.Collections.Interfaces
+{
+    public interface IAsyncQueue<T>
+    {
+        int Count { get; }
+
+        bool IsEmpty { get; }
+
+        Task Enqueue(T item);
+
+        Task<T> Dequeue();
+
+        Task<T> Peek();
+    }
+}
