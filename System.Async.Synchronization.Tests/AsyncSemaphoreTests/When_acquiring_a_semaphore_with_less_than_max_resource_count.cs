488bddf852faf1ddc587eef606dc3c3bfa3545c6
Gary <garyrday@outlook.com>
2017-02-09 06:51:21 +0000

    Added a few more tests

---
+++ System.Async.Synchronization.Tests/AsyncSemaphoreTests/When_acquiring_a_semaphore_with_less_than_max_resource_count.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Async.Synchronization.Tests.AsyncSemaphoreTests
+{
+    [Subject(typeof(AsyncSemaphore))]
+    class When_acquiring_a_semaphore_with_less_than_max_resource_count
+    {
+        protected static AsyncSemaphore Subject;
+        protected static Task Result;
+
+        Establish context = () =>
+        {
+            Subject = new AsyncSemaphore(2);
+        };
+
+        Cleanup after = () =>
+        {
+        };
+
+        Because of = () =>
+        {
+            Result = Subject.WaitAsync();
+        };
+
+        It should_acquire_the_lock = () => Result.IsCompleted.ShouldBeTrue();
+    }
+}
---
+++ System.Async.Synchronization.Tests/AsyncSemaphoreTests/When_acquiring_a_semaphore_with_no_free_resources.cs
@@ -0,0 +1,34 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Async.Synchronization.Tests.AsyncSemaphoreTests
+{
+    [Subject(typeof(AsyncSemaphore))]
+    class When_acquiring_a_semaphore_with_no_free_resources
+    {
+        protected static AsyncSemaphore Subject;
+        protected static Task Blocking;
+        protected static Task Result;
+
+        Establish context = () =>
+        {
+            Subject = new AsyncSemaphore(1);
+            Blocking = Subject.WaitAsync();
+        };
+
+        Cleanup after = () =>
+        {
+        };
+
+        Because of = () =>
+        {
+            Result = Subject.WaitAsync(10);
+        };
+
+        It should_acquire_the_lock = () => Result.IsCompleted.ShouldBeFalse();
+    }
+}
